1731d13c6a266695a11a1827050d53beee69e65d
Roberto Carlos <betocarlos00@hotmail.com>
2022-01-31 01:35:02 -0300

    imgs, search, litle style

--- PokeSharp/Controllers/PokemonController.cs
+++ PokeSharp/Controllers/PokemonController.cs
@@ -3,37 +3,56 @@ using PokeSharp.Models.Pokemon;
 using RestSharp;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq;
 
 namespace PokeSharp.Controllers
 {
     public class PokemonController : Controller
     {
-        //public async Task<IActionResult> Index()
-        //{
-        //    return View();
-        //}
-        
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
+            ViewData["CurrentFilter"] = searchString;
+
             PokeList pokeList = new PokeList();
 
-            RestClient client = new RestClient("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
-
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/pokemon?limit=24&offset=0");
             RestRequest request = new RestRequest();
-
             RestResponse response = await client.ExecuteAsync(request);
-            //string output = JsonConvert.SerializeObject(response.Content);
 
-
-            if(response.ContentLength <= -1 || response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            if (response.ContentLength <= -1 || response.StatusCode == System.Net.HttpStatusCode.BadRequest)
             {
                 ModelState.AddModelError("", "Erro!");
             }else{
                 pokeList = JsonConvert.DeserializeObject<PokeList>(response.Content);
-            }
 
-            return View(pokeList.Pokemons);
+                string[] imgs = new string[pokeList.Pokemons.Count()];
+                int count = 1;
+                foreach (var item in pokeList.Pokemons)
+                {
+                    string temp = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + count + ".png";
+
+                    imgs[count-1] = temp;
+                    count++;
+                }
+                ViewBag.imgUrl = imgs;
+
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    pokeList.Pokemons = pokeList.Pokemons.Where(s => s.Name.Contains(searchString));
+                }
+            }
+            return View(pokeList);
+        }
+        public IActionResult Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var pokemon = new Pokemon();
+
+            return View(pokemon);
         }
     }
 }
--- PokeSharp/Models/Pokemon/Pokemon.cs
+++ PokeSharp/Models/Pokemon/Pokemon.cs
@@ -5,6 +5,12 @@ namespace PokeSharp.Models.Pokemon
 {
     public class Pokemon
     {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("front_default")]
+        public Uri Img { get; set; }
+
         [JsonProperty("name")]
         public string Name { get; set; }
 
