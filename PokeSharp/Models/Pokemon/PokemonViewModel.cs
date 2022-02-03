7c5276bdc17babc81eb61d66dae7b56503ce6dbf
Roberto Carlos <betocarlos00@hotmail.com>
2022-02-03 13:10:33 -0300

    datails and types

--- PokeSharp/Models/Pokemon/PokemonViewModel.cs
+++ PokeSharp/Models/Pokemon/PokemonViewModel.cs
@@ -7,10 +7,22 @@ namespace PokeSharp.Models.Pokemon
     public class PokemonViewModel
     {
         [JsonProperty("abilities")]
-        public IEnumerable<AbilitiesViewModel> Abilities { get; set; }
+        public IEnumerable<Abilities> Abilities { get; set; }
 
         [JsonProperty("base_experience")]
         public int BaseExp { get; set;}
+
+        [JsonProperty("forms")]
+        public IEnumerable<Species> Forms { get; set; }
+
+        [JsonProperty("game_indices")]
+        public IEnumerable<GameIndex> GameIndices { get; set; }
+
+        [JsonProperty("location_area_encounters")]
+        public string LocationAreaEncounters { get; set; }
+
+        [JsonProperty("moves")]
+        public IEnumerable<Move> Moves { get; set; }
 
         [JsonProperty("height")]
         public int Height { get; set; }
@@ -24,13 +36,338 @@ namespace PokeSharp.Models.Pokemon
         [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("species")]
+        public Species Species { get; set; }
+
+        [JsonProperty("sprites")]
+        public Sprites Sprites { get; set; }
+
+        [JsonProperty("stats")]
+        public IEnumerable<Stat> Stats { get; set; }
+
+        [JsonProperty("types")]
+        public IEnumerable<TypeElement> Types { get; set; }
+
         [JsonProperty("order")]
         public int Order { get; set; }
 
         [JsonProperty("url")]
-        public Uri Url { get; set; }
+        public string Url { get; set; }
 
         [JsonProperty("wight")]
         public int Weight { get; set; }
+    }
+
+    public class Abilities
+    {
+        [JsonProperty("ability")]
+        public Species Ability { get; set; }
+
+        [JsonProperty("is_hidden")]
+        public bool IsHidden { get; set; }
+
+        [JsonProperty("slot")]
+        public long Slot { get; set; }
+    }
+    public class Species
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+    public class GameIndex
+    {
+        [JsonProperty("game_index")]
+        public long GameIndexGameIndex { get; set; }
+
+        [JsonProperty("version")]
+        public Species Version { get; set; }
+    }
+    public partial class Move
+    {
+        [JsonProperty("move")]
+        public Species MoveMove { get; set; }
+
+        [JsonProperty("version_group_details")]
+        public IEnumerable<VersionGroupDetail> VersionGroupDetails { get; set; }
+    }
+    public class VersionGroupDetail
+    {
+        [JsonProperty("level_learned_at")]
+        public long LevelLearnedAt { get; set; }
+
+        [JsonProperty("move_learn_method")]
+        public Species MoveLearnMethod { get; set; }
+
+        [JsonProperty("version_group")]
+        public Species VersionGroup { get; set; }
+    }
+    public partial class GenerationV
+    {
+        [JsonProperty("black-white")]
+        public Sprites BlackWhite { get; set; }
+    }
+
+    public class GenerationIv
+    {
+        [JsonProperty("diamond-pearl")]
+        public Sprites DiamondPearl { get; set; }
+
+        [JsonProperty("heartgold-soulsilver")]
+        public Sprites HeartgoldSoulsilver { get; set; }
+
+        [JsonProperty("platinum")]
+        public Sprites Platinum { get; set; }
+    }
+
+    public class Versions
+    {
+        [JsonProperty("generation-i")]
+        public GenerationI GenerationI { get; set; }
+
+        [JsonProperty("generation-ii")]
+        public GenerationIi GenerationIi { get; set; }
+
+        [JsonProperty("generation-iii")]
+        public GenerationIii GenerationIii { get; set; }
+
+        [JsonProperty("generation-iv")]
+        public GenerationIv GenerationIv { get; set; }
+
+        [JsonProperty("generation-v")]
+        public GenerationV GenerationV { get; set; }
+
+        [JsonProperty("generation-vi")]
+        public Dictionary<string, Home> GenerationVi { get; set; }
+
+        [JsonProperty("generation-vii")]
+        public GenerationVii GenerationVii { get; set; }
+
+        [JsonProperty("generation-viii")]
+        public GenerationViii GenerationViii { get; set; }
+    }
+
+    public class Sprites
+    {
+        [JsonProperty("back_default")]
+        public string BackDefault { get; set; }
+
+        [JsonProperty("back_female")]
+        public object BackFemale { get; set; }
+
+        [JsonProperty("back_shiny")]
+        public string BackShiny { get; set; }
+
+        [JsonProperty("back_shiny_female")]
+        public object BackShinyFemale { get; set; }
+
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
+
+        [JsonProperty("front_female")]
+        public object FrontFemale { get; set; }
+
+        [JsonProperty("front_shiny")]
+        public string FrontShiny { get; set; }
+
+        [JsonProperty("front_shiny_female")]
+        public object FrontShinyFemale { get; set; }
+
+        [JsonProperty("other", NullValueHandling = NullValueHandling.Ignore)]
+        public Other Other { get; set; }
+
+        [JsonProperty("versions", NullValueHandling = NullValueHandling.Ignore)]
+        public Versions Versions { get; set; }
+
+        [JsonProperty("animated", NullValueHandling = NullValueHandling.Ignore)]
+        public Sprites Animated { get; set; }
+    }
+
+    public class GenerationI
+    {
+        [JsonProperty("red-blue")]
+        public RedBlue RedBlue { get; set; }
+
+        [JsonProperty("yellow")]
+        public RedBlue Yellow { get; set; }
+    }
+
+    public class RedBlue
+    {
+        [JsonProperty("back_default")]
+        public string BackDefault { get; set; }
+
+        [JsonProperty("back_gray")]
+        public string BackGray { get; set; }
+
+        [JsonProperty("back_transparent")]
+        public string BackTransparent { get; set; }
+
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
+
+        [JsonProperty("front_gray")]
+        public string FrontGray { get; set; }
+
+        [JsonProperty("front_transparent")]
+        public string FrontTransparent { get; set; }
+    }
+
+    public class GenerationIi
+    {
+        [JsonProperty("crystal")]
+        public Crystal Crystal { get; set; }
+
+        [JsonProperty("gold")]
+        public Gold Gold { get; set; }
+
+        [JsonProperty("silver")]
+        public Gold Silver { get; set; }
+    }
+
+    public class Crystal
+    {
+        [JsonProperty("back_default")]
+        public string BackDefault { get; set; }
+
+        [JsonProperty("back_shiny")]
+        public string BackShiny { get; set; }
+
+        [JsonProperty("back_shiny_transparent")]
+        public string BackShinyTransparent { get; set; }
+
+        [JsonProperty("back_transparent")]
+        public string BackTransparent { get; set; }
+
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
+
+        [JsonProperty("front_shiny")]
+        public string FrontShiny { get; set; }
+
+        [JsonProperty("front_shiny_transparent")]
+        public string FrontShinyTransparent { get; set; }
+
+        [JsonProperty("front_transparent")]
+        public string FrontTransparent { get; set; }
+    }
+
+    public class Gold
+    {
+        [JsonProperty("back_default")]
+        public string BackDefault { get; set; }
+
+        [JsonProperty("back_shiny")]
+        public string BackShiny { get; set; }
+
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
+
+        [JsonProperty("front_shiny")]
+        public string FrontShiny { get; set; }
+
+        [JsonProperty("front_transparent", NullValueHandling = NullValueHandling.Ignore)]
+        public string FrontTransparent { get; set; }
+    }
+
+    public class GenerationIii
+    {
+        [JsonProperty("emerald")]
+        public Emerald Emerald { get; set; }
+
+        [JsonProperty("firered-leafgreen")]
+        public Gold FireredLeafgreen { get; set; }
+
+        [JsonProperty("ruby-sapphire")]
+        public Gold RubySapphire { get; set; }
+    }
+
+    public class Emerald
+    {
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
+
+        [JsonProperty("front_shiny")]
+        public string FrontShiny { get; set; }
+    }
+
+    public class Home
+    {
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
+
+        [JsonProperty("front_female")]
+        public object FrontFemale { get; set; }
+
+        [JsonProperty("front_shiny")]
+        public string FrontShiny { get; set; }
+
+        [JsonProperty("front_shiny_female")]
+        public object FrontShinyFemale { get; set; }
+    }
+
+    public class GenerationVii
+    {
+        [JsonProperty("icons")]
+        public DreamWorld Icons { get; set; }
+
+        [JsonProperty("ultra-sun-ultra-moon")]
+        public Home UltraSunUltraMoon { get; set; }
+    }
+
+    public class DreamWorld
+    {
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
+
+        [JsonProperty("front_female")]
+        public object FrontFemale { get; set; }
+    }
+
+    public class GenerationViii
+    {
+        [JsonProperty("icons")]
+        public DreamWorld Icons { get; set; }
+    }
+
+    public class Other
+    {
+        [JsonProperty("dream_world")]
+        public DreamWorld DreamWorld { get; set; }
+
+        [JsonProperty("home")]
+        public Home Home { get; set; }
+
+        [JsonProperty("official-artwork")]
+        public OfficialArtwork OfficialArtwork { get; set; }
+    }
+
+    public class OfficialArtwork
+    {
+        [JsonProperty("front_default")]
+        public string FrontDefault { get; set; }
+    }
+
+    public class Stat
+    {
+        [JsonProperty("base_stat")]
+        public long BaseStat { get; set; }
+
+        [JsonProperty("effort")]
+        public long Effort { get; set; }
+
+        [JsonProperty("stat")]
+        public Species StatStat { get; set; }
+    }
+
+    public class TypeElement
+    {
+        [JsonProperty("slot")]
+        public long Slot { get; set; }
+
+        [JsonProperty("type")]
+        public Species Type { get; set; }
     }
 }
